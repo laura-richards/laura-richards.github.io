43e706e12958130e9652db9feb1c4055f453dad8
Laura Richards <lionheartnz85@gmail.com>
2023-06-05 02:34:54 +1200

    Add blog details and images

---
+++ blog/tech-blogs/javascript -dom-code-examples.js
@@ -0,0 +1,56 @@
+// //--DOING THE WASHING--//
+
+// //--A LOAD OF TOWELS--//
+// for (i = 0; i <= washingPile.length; i++) {
+//   foldTowel()
+// }
+
+// //--CLOTHES AND TOWELS--//
+// for (i = 0; i <= washingPile.length; i++) {
+//   if (item === towel) {
+//     foldTowel()
+//   } else {
+//     placeToSide()
+//   }
+// }
+
+// //--FOLDING MOUNT-WASHMORE--//
+// for (i = 0; i <= washingPile.length; i++) {
+//   if (item === towel) {
+//     foldTowel()
+//   } else if (item == child1) {
+//     placeInTub(child1)
+//   } else if (item == child2) {
+//     placeInTub(child2)
+//   }
+// }
+
+//--WORKING WITH AN ARRAY--//
+// Create an array of snacks
+let ourSnacks = ['chips', 'apples', 'chocolate', 'pea snaps']
+//Retrieve the 3rd item in the array
+console.log(ourSnack[2]) // in the console you would see 'chocolate'
+
+//--OBJECT--//
+// Create an Object party with a date and time
+let party = {
+  time: '2pm',
+  date: '20th June',
+}
+//retrieve the time value
+console.log(party.time)
+
+// //--FUNCTIONS--//
+// // //Functions structure
+// function functionName(parameter) {
+//   do something
+// }
+
+// //Write a function for folding a towel
+// function foldTowel() {
+//   console.log('fold towel in half lengthways')
+//   console.log('fold towel in half widthways')
+//   console.log('fold towel in thirds lengthways')
+//   console.log('place towel in towel pile')
+// }
+// //Well this actually prints the instructions to the console
